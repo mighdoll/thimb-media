e0c8889664fd9032bc4925dfe01ed6d9d30d5e14
lee mighdoll <lee@underneath.ca>
2023-02-08 21:04:12 -0800

    mv functions dir up one level

--- deploy/functions/_middleware.ts
+++
@@ -1,8 +0,0 @@
-// allow CORS requests
-export const onRequest: PagesFunction = async ({ next }) => {
-  const response = await next();
-  response.headers.set('Access-Control-Allow-Origin', '*');
-  response.headers.set('Access-Control-Max-Age', '86400');
-  console.log("Middleware: Setting CORS headers", response.headers);
-  return response;
-};
